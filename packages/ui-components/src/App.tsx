5f03151644cae5af23088db7b2b06ba7067fa1d1
Rakesh Pulli <45898196+RakeshP10@users.noreply.github.com>
2022-11-04 12:52:39 +0530

    Initial Project Setup

---
+++ packages/ui-components/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from './components/Button';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button textColor="blue">Click me!</Button>
+    </div>
+  );
+}
+
+export default App;
