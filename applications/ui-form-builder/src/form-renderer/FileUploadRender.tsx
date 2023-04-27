235b01e95eab37905d225e21a9d055c7efede9b4
Rakesh Pulli <rakesh.pulli@blueconchtech.com>
2023-04-27 15:47:09 +0530

    Added code for File Upload

--- applications/ui-form-builder/src/form-renderer/FileUploadRender.tsx
+++ applications/ui-form-builder/src/form-renderer/FileUploadRender.tsx
@@ -1,10 +1,5 @@
-import {
-  Button,
-  FileUpload,
-  Stack,
-} from "@power-form-builder/ui-components";
+import { Button, FileUpload, Stack } from "@power-form-builder/ui-components";
 import { ChangeEvent, useState } from "react";
-
 
 type Props = {
   onChange: any;
@@ -23,8 +18,27 @@ const FileUploadRender = () => {
       return;
     }
 
-    // handle file upload logic here
+    // Create a FormData object to send the file to the server
+    const formData = new FormData();
+    formData.append("file", selectedFile);
+
+    // Make an API request to upload the file
+    fetch("http://localhost:4000/api/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("File uploaded successfully");
+        } else {
+          console.error("Failed to upload file");
+        }
+      })
+      .catch((error) => {
+        console.error("Failed to upload file", error);
+      });
   };
+  
   return (
     <Stack direction="row" alignItems="center" spacing={2}>
       <FileUpload name={"dfdf"} required={true} onChange={handleFileSelect} />
